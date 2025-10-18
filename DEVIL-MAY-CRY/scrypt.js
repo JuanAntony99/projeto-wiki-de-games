7a9ca96ebe5d62d3b2a7ac3ecb6083cbdf64ca5f
JuanAntony99 <juanantony.nascimento5@gmail.com>
2025-10-18 19:17:13 -0300

    primeiro commit do projeto wiki

---
+++ DEVIL-MAY-CRY/scrypt.js
@@ -0,0 +1,13 @@
+const fadeIns = document.querySelectorAll(".fade-in");
+
+function checkFade() {
+  fadeIns.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      section.classList.add("visible");
+    }
+  });
+}
+
+window.addEventListener("scroll", checkFade);
+window.addEventListener("load", checkFade);
