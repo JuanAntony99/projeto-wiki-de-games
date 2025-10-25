354a0eea19cf90c706e7745016cd42a30d4b376a
JuanAntony99 <juanantony.nascimento5@gmail.com>
2025-10-25 11:42:22 -0300

    adicionando validação de formulário e mensagens de erro no cadastro

--- TELA-CADASTRO/scrypt.js
+++ TELA-CADASTRO/scrypt.js
@@ -1,1 +1,39 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+document.getElementById("cadastro-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let valido = true;
+
+    const nomePessoa = document.getElementById("nomePessoa");
+    const email = document.getElementById("email");
+    const senha = document.getElementById("senha");
+
+    const erroNome = document.getElementById("erroNome");
+    const erroEmail = document.getElementById("erroEmail");
+    const erroSenha = document.getElementById("erroSenha");
+
+    erroNome.textContent = "";
+    erroEmail.textContent = "";
+    erroSenha.textContent = "";
+
+    if (nomePessoa.value.trim() === "" ) {
+        erroNome.textContent = "Por favor, insira seu nome.";
+        valido = false;
+    }
+
+    const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailValido.test(email.value)) {
+        erroEmail.textContent = "Por favor, insira um email válido. (ex. exemplo123@gmail.com).";
+        valido = false;
+    }
+
+    if (senha.value.lenght = 8) {
+        erroSenha.textContent = "A senha deve ter pelo menos 8 caracteres.";
+        valido = false;
+    }
+
+    if(valido) {
+        alert("Cadastro finalizado com sucesso!");
+    }
+});
